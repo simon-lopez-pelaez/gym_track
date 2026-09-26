10d81889b22b0d59ec501d10319e7abb028b7447
kevinj18 <kevinjimenez839@gmail.com>
2026-09-25 20:46:36 -0500

    se crea un estado para la pantalla de horarios, que es un StatefulWidget, ya que se necesita actualizar la interfaz cuando se seleccionan o deseleccionan los días de entrenamiento. También se comienza a programar el appBar y el body

--- lib/pantallas/horarios.dart
+++ lib/pantallas/horarios.dart
@@ -1,14 +1,33 @@
-// lib/pantallas/login.dart
 import 'package:flutter/material.dart';
+import 'detalle_rutina.dart';
 
-class HorariosScreen extends StatelessWidget {
+class HorariosScreen extends StatefulWidget {
   const HorariosScreen({super.key});
+
+  @override
+  State<HorariosScreen> createState() => _HorariosScreenState();
+}
+
+class _HorariosScreenState extends State<HorariosScreen> {
+  final List<String> dias = [
+    'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'
+  ];
+  List<bool> diasSeleccionados = [true, false, true, false, true, false, false];
+
+  final List<String> grupoMuscular = [
+    'Pecho', 'Descanso', 'Espalda', 'Descanso', 'Pierna', 'Descanso', 'Descanso'
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('P-04 Horarios')),
-      body: const Center(child: Text('Pantalla de horarios')),
+      body: ListView.builder(
+        itemCount: dias.length,
+        itemBuilder: (context, index) {
+          bool activo = diasSeleccionados[index];
+        },
+      ),
     );
   }
 }
